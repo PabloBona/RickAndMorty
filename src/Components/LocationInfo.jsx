b01ab805d48dc0cc78ce315a58b1d76a9125d57f
PabloBona <110569486+PabloBona@users.noreply.github.com>
2022-12-04 18:18:14 -0300

    First

--- src/Components/LocationInfo.jsx
+++ src/Components/LocationInfo.jsx
@@ -5,10 +5,10 @@ const Location = ({location}) => {
     <article className='mainInfo'>
      
     <ul className='ulheader'>
-        <li className='ulheader__li'>Name:  <br /> <span className='ulheader__span' >{location?.name}</span></li>
-        <li className='ulheader__li'>Type: <br /> <span className='ulheader__span' >{location?.type}</span></li>
-        <li className='ulheader__li'>Dimension: <br /> <span className='ulheader__span' > {location?.dimension}</span></li>
-        <li className='ulheader__li'>Population: <br /> <span className='ulheader__span' >{location?.residents.length}</span></li>
+        <li className='ulheader__li item1'>Name:  <br /> <span className='ulheader__span item1' >{location?.name}</span></li>
+        <li className='ulheader__li item2'>Type: <br /> <span className='ulheader__span item2' >{location?.type}</span></li>
+        <li className='ulheader__li item3'>Dimension: <br /> <span className='ulheader__span item3' > {location?.dimension}</span></li>
+        <li className='ulheader__li item4'>Population: <br /> <span className='ulheader__span item4' >{location?.residents.length}</span></li>
     </ul>
     </article>
   )
